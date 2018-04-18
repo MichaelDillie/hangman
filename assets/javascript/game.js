b19d1e9b450e2778661a9141902dd1484083404f
Michael Dillie <michaeldillie@gmail.com>
2018-04-18 12:47:58 -0500

    Adding some js (Not Functional)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,35 @@
 var guessesLeft = 15;
 var word;
+var wordToGuess = [];
 var guessedLetters = [];
 var lost = "lost";
 
 var wordList = ["javascript", "array", "document", "element", "argument", "function", "variable", "program", "developer", "internet", "coffee", "sleep"];
 
-// Word picked
-// document.getElementById("play-btn").addEventListener("click", playGame);
-function playGame() {
-    word = wordList[Math.floor(Math.random() * wordList.length)];
+function start() {
+    var displayWord = [];
+
+    //Randomly picks one word from wordList
+    word = wordList[Math.floor(Math.random() * wordList.length)]
     console.log(word);
-}
-playGame();
 
-document.onkeyup = function (event) {
-    console.log(event.key);
+    //Splitting word by each character and placing it into the empty wordToGuess array
+    wordToGuess = word.split("");
+    console.log(wordToGuess.length);
 
-    // Guesses left
-    guessesLeft--;
-    document.getElementById("guesses-left").innerHTML = guessesLeft;
-    console.log(guessesLeft);
-    if (guessesLeft === 0) {
-        alert("You Lost :(")
+    //Creating the blank spaces for the guessable word
+    for(var i = 0; i < wordToGuess.length; i++) {
+        displayWord.push("_");
     }
 
-    // Letters guessed
-    guessedLetters.push(event.key);
-    document.getElementById("letters-guessed").innerHTML = guessedLetters;
-    console.log(guessedLetters);
+    document.onkeyup = function(event) {
+        //Starts game
+        if(event.keyCode === 13) {
+            //Changing the HTML from "Press Enter To Start!!" to "Can You Guess This Word?"
+            document.getElementById("enter-to-start").innerHTML = "Can You Guess This Word?";
+            document.getElementById("word-to-guess").innerHTML = displayWord;
+            
+        }
+    }
 }
+start();
